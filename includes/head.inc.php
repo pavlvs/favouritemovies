b44a1fb03f29793c095436b6f458d1d0a923ac8d
Paul J Shaw <pavlvsxavier@gmail.com>
2020-01-16 12:54:53 +0000

    made loading of scripts conditional on active page in head.inc.php

--- includes/head.inc.php
+++ includes/head.inc.php
@@ -9,12 +9,29 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script>var $movieID  = "<?php echo $movieID; ?>"</script>
-    <script>var $userID  = "<?php echo $userID; ?>"</script>
-    <script src = "scripts/add-favs.js"></script>
-    <script src = "scripts/add-single-favs.js"></script>
-    <script src = "scripts/remove-favs.js"></script>
-    <script src = "scripts/remove-single-favs.js"></script>
+
     <script src = "scripts/navigation.js"></script>
+
+<?php
+if (isset($userID)) {
+	?>
+    <script>var $userID  = "<?php echo $userID; ?>"</script>
+	<script src = "scripts/add-favs.js"></script>
+	<script src = "scripts/remove-favs.js"></script>
+
+    <?php
+if (isset($movieID)) {?>
+        <script src = "scripts/add-single-favs.js"></script>
+        <script src = "scripts/remove-single-favs.js"></script>
+    <?php }
+
+	if (isset($page)) {
+		// Admin scripts go here
+
+	}
+
+}
+?>
 
     <title>Instant Update Database Project</title>
 </head>
